397b97eb76e432910c1619676d7dd47317f2d9fa
AdelsGuerr24 <47429845+adelsguerr@users.noreply.github.com>
2021-05-21 12:48:30 -0600

    Update claseMenu.cs

--- SistemaRestaurante/Clases/claseMenu.cs
+++ SistemaRestaurante/Clases/claseMenu.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +12,9 @@ namespace Proyecto_Final_Periodo3.Clases
         private string nombre;
         private string tipo;
         private string precio;
-
-
+        
         public claseMenu()
         {
-
         }
 
         public string Nombre { get => nombre; set => nombre = value; }
