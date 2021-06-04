719a7dce3d69c2d50e4df326ae54ddc4dc878851
AlexbyX <alexguerrero3a91@gmail.com>
2021-06-04 12:28:18 -0600

    conexion a la base

---
+++ SistemaRestaurante/conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Proyecto_Final_Periodo3
+{
+    class conexion
+    {
+        public static SqlConnection Conectar()
+        {
+            SqlConnection cn = new SqlConnection("ALEXBY; DATABASE=RestauranteBD; Integrated security=true");
+            cn.Open();
+
+            return cn;
+        }
+    }
+}
